a308b9391ccb6346d07eaac8bda77cfdbe825889
Mikhail <mixa.penkin@gmail.com>
2023-11-26 21:29:41 +0300

    deploy gh-pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-			'~icons': path.resolve(__dirname, 'node_modules/bootstrap-icons'),
-    },
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+			'~icons': path.resolve(__dirname, 'node_modules/bootstrap-icons')
+		}
+	},
+	publicPath: `/vue-split-bill-app/`
 })
